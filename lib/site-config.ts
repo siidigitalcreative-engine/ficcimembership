35b64efb8b9ed98cf8e07a7332835f15b7daca0c
siidigitalcreative-engine <siidigitalcreative@gmail.com>
2026-07-28 14:18:17 +0800

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,11 +1,19 @@
 export const siteConfig = {
   organizationName: process.env.NEXT_PUBLIC_ORGANIZATION_NAME || "FICCI",
+  programName:
+    process.env.NEXT_PUBLIC_PROGRAM_NAME ||
+    "Membership Discount Benefits Program",
   supportEmail:
     process.env.NEXT_PUBLIC_SUPPORT_EMAIL || "membership@ficci.org.ph",
-  supportPhone: process.env.NEXT_PUBLIC_SUPPORT_PHONE || "",
+  supportPhone: process.env.NEXT_PUBLIC_SUPPORT_PHONE || "+63 917 632 5107",
   benefitsDirectoryUrl:
-    process.env.NEXT_PUBLIC_BENEFITS_DIRECTORY_URL || "",
+    process.env.NEXT_PUBLIC_BENEFITS_DIRECTORY_URL || "/partners",
   myGlueDownloadUrl:
     process.env.NEXT_PUBLIC_MY_GLUE_DOWNLOAD_URL ||
     "https://www.glueup.com/download-my-glue",
+  glueUpProgramUrl:
+    process.env.NEXT_PUBLIC_GLUEUP_PROGRAM_URL ||
+    "https://app.glueup.com/event/membership-discount-benefits-program-177531/",
+  programStartDate: process.env.NEXT_PUBLIC_PROGRAM_START_DATE || "2026-04-06",
+  programEndDate: process.env.NEXT_PUBLIC_PROGRAM_END_DATE || "2027-02-28",
 };
